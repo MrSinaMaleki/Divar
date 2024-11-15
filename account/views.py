86f12d264d31f51b5887776f147535ad377ca93a
MrSinaMaleki <sina67631@gmail.com>
2024-11-15 23:36:32 +0330

    temp profile api view

--- account/views.py
+++ account/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import UserSerializer
+
+
+@api_view(['GET'])
+def profile(request):
+    user = request.user
+    serialized_user = UserSerializer(user).data
+    return Response({'user': serialized_user })
