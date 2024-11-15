c4b3690bbb04011cf85e4e116a110c89198ec6c7
MrSinaMaleki <sina67631@gmail.com>
2024-11-16 00:33:34 +0330

    refrsh token api setup

--- account/urls.py
+++ account/urls.py
@@ -1,3 +1,5 @@
+from termios import PARMRK
+
 from django.urls import path
 
 from account import views
@@ -6,4 +8,5 @@ from account.views import profile
 urlpatterns = [
     path('profile', profile, name='profile'),
     path('login', views.Login.as_view(), name='login'),
+    path('refrshtoken', views.RefreshToken.as_view(), name='refrshtoken'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,20 +1,22 @@
 
-from .serializers import UserSerializer
-from django.contrib.auth import get_user_model
-from rest_framework.response import Response
-from rest_framework import exceptions
-from rest_framework.permissions import AllowAny
-from rest_framework.decorators import api_view, permission_classes
-from django.views.decorators.csrf import ensure_csrf_cookie
 from account.serializers import UserSerializer
 from account.utils import generate_access_token, generate_refresh_token
 from rest_framework.views import APIView
+import jwt
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.views.decorators.csrf import csrf_protect
+from rest_framework import exceptions
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from account.utils import generate_access_token
 
 
 @api_view(['GET'])
 def profile(request):
     user = request.user
+    print("user => ",user)
     serialized_user = UserSerializer(user).data
     return Response({'user': serialized_user })
 
@@ -25,8 +27,6 @@ class Login(APIView):
     serializer_class = UserSerializer
 
     def post(self, request):
-        print("hello ?")
-        print(request.data)
         User = get_user_model()
         email = request.data.get('email')
         password = request.data.get('password')
@@ -44,4 +44,43 @@ class Login(APIView):
             'access_token': access_token,
             # 'user': serialized_user,
         })
+
+
+# @csrf_protect
+class RefreshToken(APIView):
+    permission_classes = [AllowAny,]
+    serializer_class = UserSerializer
+
+    """
+        To obtain a new access_token this view expects 2 important things:
+            1. a cookie that contains a valid refresh_token
+            2. a header 'X-CSRFTOKEN' with a valid csrf token, client app can get it from cookies "csrftoken"
+    """
+
+    def get(self, request):
+
+        User = get_user_model()
+        refresh_token = request.COOKIES.get('refreshtoken')
+        print(refresh_token)
+
+        if refresh_token is None:
+            raise exceptions.AuthenticationFailed(
+                'Authentication credentials were not provided.')
+        try:
+            payload = jwt.decode(
+                refresh_token, settings.SECRET_KEY, algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise exceptions.AuthenticationFailed(
+                'expired refresh token, please login again.')
+
+        user = User.objects.filter(id=payload.get('user_id')).first()
+        if user is None:
+            raise exceptions.AuthenticationFailed('User not found')
+
+        if not user.is_active:
+            raise exceptions.AuthenticationFailed('user is inactive')
+
+        access_token = generate_access_token(user)
+        return Response({'access_token': access_token})
+
 
